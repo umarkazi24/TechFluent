eb05c58fbaeb82a429c71d49d15b8f314a725928
umarkazi24 <umarkazi54@gmail.com>
2024-10-10 19:47:59 -0500

    Second commit for updating profile

--- src/main/java/org/studyeasy/SpringStarter/Controller/AccountController.java
+++ src/main/java/org/studyeasy/SpringStarter/Controller/AccountController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,55 +18,83 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @Controller
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
-    
+
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(Model model) {
 
         Account account = new Account();
         model.addAttribute("account", account);
         return "account_views/register";
     }
-    
-   @PostMapping("/register")
-   public String register_user(@Valid @ModelAttribute Account account, BindingResult result) {
 
-        if(result.hasErrors()){
+    @PostMapping("/register")
+    public String register_user(@Valid @ModelAttribute Account account, BindingResult result) {
+
+        if (result.hasErrors()) {
             return "account_views/register";
         }
-        
-       accountService.save(account);
-       return "redirect:/";
-   }
 
-   @GetMapping("/login")
-   public String login(Model model) {
-       return "account_views/login";
-   }
+        accountService.save(account);
+        return "redirect:/";
+    }
 
-   @GetMapping("/profile")
-   @PreAuthorize("isAuthenticated()")
-   public String profile(Model model, Principal principal) {
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "account_views/login";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public String profile(Model model, Principal principal) {
         String authUser = "email";
-        if(principal != null){
+        if (principal != null) {
             authUser = principal.getName();
         }
         Optional<Account> optionalAccount = accountService.findOneByEmail(authUser);
-        if(optionalAccount.isPresent()){
+        if (optionalAccount.isPresent()) {
             Account account = optionalAccount.get();
             model.addAttribute("account", account);
             model.addAttribute("photo", account.getPhoto());
             return "account_views/profile";
+        } else {
+            return "redirect:/?error";
+        }
+    }
+
+    @PostMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public String post_profile(@Valid @ModelAttribute Account account, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "account_views/profile";
+        }
+        String authUser = "email";
+        if (principal != null) {
+            authUser = principal.getName();
+        }
+        Optional<Account> optionalAccount = accountService.findOneByEmail(authUser);
+        if(optionalAccount.isPresent()){
+
+            Account account_by_id = accountService.findById(account.getId()).get();
+            account_by_id.setAge(account.getAge());
+            account_by_id.setDate_of_birth(account.getDate_of_birth());
+            account_by_id.setFirstname(account.getFirstname());
+            account_by_id.setGender(account.getGender());
+            account_by_id.setLastname(account.getLastname());
+            account_by_id.setPassword(account.getPassword());
+
+            accountService.save(account_by_id);
+            SecurityContextHolder.clearContext();
+            return "redirect:/logout";
+
         }else{
             return "redirect:/?error";
         }
-   }
-   
-    
+        
+    }
+
 }
--- src/main/java/org/studyeasy/SpringStarter/services/AccountService.java
+++ src/main/java/org/studyeasy/SpringStarter/services/AccountService.java
@@ -84,4 +84,8 @@ public class AccountService implements UserDetailsService {
     public Optional<Account> findOneByEmail(String email){
         return accountRepository.findOneByEmailIgnoreCase(email);
     }
+
+    public Optional<Account> findById(long id){
+        return accountRepository.findById(id);
+    }
 }
